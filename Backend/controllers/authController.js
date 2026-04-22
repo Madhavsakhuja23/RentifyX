1464b8e4133e6f8152c9f91ff8e8941f455dd34b
Madhav26 <msakhuja22@gmail.com>
2026-04-22 20:54:21 +0530

    Backend Updated

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -25,7 +25,7 @@ export const signupUser = async (req, res) => {
       return res.status(400).json({ msg: "User already exists" });
     }
 
-   
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
@@ -85,27 +85,32 @@ export const loginUser = async (req, res) => {
 
 export const googleAuth = async (req, res) => {
   try {
-    const { name, email, photo } = req.body;
+    const { name, email, photo, role } = req.body;
 
     let user = await User.findOne({ email });
 
-    // If user doesn't exist → create
-    if (!user) {
-      user = new User({
+    if (user) {
+      if (role && user.role !== role) {
+        user.role = role;
+        await user.save();
+
+        // fetch latest updated user
+        user = await User.findOne({ email });
+      }
+    } else {
+      user = await User.create({
         name,
         email,
-        password: "google-auth", // dummy password
-        role: "user",
+        photo,
+        password: "google-auth",
+        role: role || "user",
       });
-
-      await user.save();
     }
 
-    // Generate JWT
     const token = jwt.sign(
       { id: user._id },
       process.env.JWT_SECRET,
-      { expiresIn: "1d" }
+      { expiresIn: "7d" }
     );
 
     res.json({
@@ -115,11 +120,13 @@ export const googleAuth = async (req, res) => {
         name: user.name,
         email: user.email,
         role: user.role,
+        photo: user.photo
       }
     });
 
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "Google auth failed" });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Google auth failed"
+    });
   }
 };
